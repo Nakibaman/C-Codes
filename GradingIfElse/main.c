121e169e10f0eb05d3dd1209bdb00050d7e8bc04
Nakibaman <nakibaman@gmail.com>
2018-02-25 22:07:30 +0600

    Sorry Update
    Minor Update

--- GradingIfElse/main.c
+++ GradingIfElse/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int marks;
+    printf("Enter the marks");
+    scanf("%d",&marks);
+    if(marks>=80)
+        printf("The Grade is A+");
+    else if(marks>=70)
+        printf("The Grade is A");
+    else if(marks>=60)
+        printf("The Grade is A-");
+    else if(marks>=50)
+        printf("The Grade is B");
+    else if(marks>=40)
+        printf("The Grade is C");
+    else if(marks>=33)
+        printf("The Grade is D");
+    else
+        printf("Failed");
     return 0;
 }
